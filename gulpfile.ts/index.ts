6579825f9cb36e664cfcc7fd8e4fcb720a381d67
mhsnet <mhsnet@163.com>
2019-04-23 14:44:21 +0800

    gulp 基础构建

--- gulpfile.ts/index.ts
+++ gulpfile.ts/index.ts
@@ -1,53 +1,11 @@
-import { dest, series, src } from 'gulp'
-import * as ts from 'gulp-typescript'
-import * as clean from 'gulp-clean';
-import * as browserify from 'browserify';
-import * as tsify from 'tsify';
-import * as source from 'vinyl-source-stream';
-import * as watchify from 'watchify';
-import * as fancy_log from 'fancy-log';
-import * as uglify from 'gulp-uglify';
-import * as sourcemaps from 'gulp-sourcemaps';
-import * as buffer from 'vinyl-buffer';
+// tp-gts/gulpfile.ts/index.ts
+import * as _ from 'lodash'
 
-const tsProject = ts.createProject("tsconfig.json");
-
-const watchedBrowserify = watchify(browserify({
-  basedir: '.',
-  debug: true,
-  entries: ['src/main.ts'],
-  cache: {},
-  packageCache: {}
-}).plugin(tsify));
-
-function taskClean(cb) {
-  return src(['dist/*'])
-    .pipe(clean());
+function taskTest(cb) {
+  let str = 'hello typescript gulp!';
+  str = _.capitalize(str);
+  console.log(`${str}`);
+  cb();
 };
 
-function taskCopyHtml() {
-  return src(['src/*.html'])
-    .pipe(dest("dist"));
-};
-
-function taskBuildJs() {
-  return watchedBrowserify
-    .transform('babelify', {
-      presets: ['es2015'],
-      extensions: ['.ts']
-    })
-    .bundle()
-    .pipe(source('bundle.js'))
-    .pipe(buffer())
-    .pipe(sourcemaps.init({ loadMaps: true }))
-    .pipe(uglify())
-    .pipe(sourcemaps.write('./'))
-    .pipe(dest("dist"));
-};
-
-const taskDefault = series(taskClean, taskCopyHtml, taskBuildJs);
-
-watchedBrowserify.on("update", taskBuildJs);
-watchedBrowserify.on("log", fancy_log);
-
-export { taskDefault as default };
+export { taskTest as default };
--- src/main.ts
+++
@@ -1,8 +0,0 @@
-import { hello } from "./hello";
-
-function showHello(divName: string, name: string) {
-  const elt = document.getElementById(divName);
-  elt.innerText = hello(name);
-}
-
-showHello("greeting", "name2");
